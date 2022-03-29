e00cae57773039fb162a237f1c96c896566bbc8f
cloudmex-alan <alanestrada@cloudmex.io>
2022-03-29 14:46:56 -0700

    ordering git

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-RUSTFLAGS='-C link-arg=-s' cargo +stable build --all --target wasm32-unknown-unknown --release
-cp -u target/wasm32-unknown-unknown/release/nft-loans.wasm res/
